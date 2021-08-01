237135a002390853cd2bd06cf7d66337bacdcccf
jiwon <jidole041214@naver.com>
2021-08-02 00:47:42 +0900

    [CREATE] INIT

---
+++ node-js/schemas/index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose.connect(
+    "mongodb://jidole02:fprhwhdk1214@localhost:27017/admin",
+    {
+      dbName: "sellery_book",
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    },
+    (err) => {
+      err
+        ? console.log("DB connecting error", err)
+        : console.log("DB connecting success!!");
+    }
+  );
+};
+
+mongoose.connection.on("error", (err) => {
+  console.error("connect err", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.error("mongo is disconnect, try to reconnect");
+  connect();
+});
+
+module.exports = connect;
