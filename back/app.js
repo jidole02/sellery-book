c65eac8763ed83031e1ee59746fb7f31310a9a3b
jiwon <jidole041214@naver.com>
2021-08-01 17:52:55 +0900

    [CREATE] INIT

---
+++ back/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pageRouter = require("./routes/page");
+
+const app = express();
+app.set("port", 8002);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", pageRouter);
+
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} router is not defined`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`waiting on ${app.get("port")} port`);
+});
