3fb0532dedc1d0c08ac7712110a4519d8913ef71
jiwon <jidole041214@naver.com>
2021-08-07 12:13:58 +0900

    [UPDATE] TOKEN CHECK

--- next-js/pages/_app.tsx
+++ next-js/pages/_app.tsx
@@ -2,8 +2,39 @@ import Head from "next/head";
 import Header from "../src/components/header";
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/dist/client/router";
+import auth from "../src/api/auth";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [login, setLogin] = useState<boolean>(false);
+  useEffect(() => {
+    const token = localStorage.getItem("sellery-token");
+    if (!token) setLogin(false);
+    else {
+      auth
+        .tokenCheck(token)
+        .then((res) => {
+          if (res.data.checked) {
+            setLogin(true);
+          } else {
+            setLogin(false);
+          }
+        })
+        .catch(() => setLogin(false));
+    }
+  }, [router.pathname]);
+  useEffect(() => {
+    const path = router.pathname;
+    if (!login) {
+      if (path.includes("/write")) {
+        router.push("/auth/login");
+        toast.info("로그인 후 이용해주세요.");
+      }
+    }
+  }, [router.pathname]);
   return (
     <>
       <Head>
