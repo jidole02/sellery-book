8abb414f034b54f74cacb2ec009b913b53f40cef
jiwon <jidole041214@naver.com>
2021-08-16 13:02:00 +0900

    [ADD] BANNER

--- next-js/pages/contest/index.tsx
+++ next-js/pages/contest/index.tsx
@@ -1,11 +1,11 @@
-import * as S from '../../pagestyle/conteststyles'
+import * as S from "../../pagestyle/conteststyles";
 
-export default function ContestPage(){
-    return(
-        <S.Wrapper>
-            <S.Container>
-        <img src="/contest.png"/>
-            </S.Container>
-        </S.Wrapper>
-    )
+export default function ContestPage() {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <img src="/contest.jpg" />
+      </S.Container>
+    </S.Wrapper>
+  );
 }
--- next-js/src/components/main/index.tsx
+++ next-js/src/components/main/index.tsx
@@ -8,13 +8,14 @@ export default function ManiPage() {
       <S.Container>
         <Top />
         <CardList title="인기작품을 만나보세요!" condition={"mostview"} />
-        <S.Slide src="/slide2.PNG" alt="" />
+        <S.Slide src="/contest.jpg" alt="" />
         <CardList title="따끈따끈한 오늘의 신작" condition={"new"} />
         <CardList title="평점이 높은 작품" condition={"mostrate"} />
-        <S.Slide src="/slide1.PNG" alt="" />
+        <S.Slide src="/recommend.jpg" alt="" />
         <CardList title="나를 위한 자기계발 도서" condition={`genre?genre=자기계발`} />
         <CardList title="세상을 바라보는 경제경영 도서" condition={`genre?genre=경제경영`} />
         <CardList title="상상에 상상을 더하다, SF판타지" condition={`genre?genre=SF판타지`} />
+        <CardList title="흥미진진한 이야기, 소설" condition={`genre?genre=소설`} />
       </S.Container>
     </S.Wrapper>
   );
