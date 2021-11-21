2947d4804ee8b89a75f2bcdda6877680e2783818
jiwon <jidole041214@naver.com>
2021-11-22 08:21:38 +0900

    [UPDATE] DOMAIN

--- next-js/src/api/axios.ts
+++ next-js/src/api/axios.ts
@@ -1,25 +1,25 @@
-import axios, { AxiosError } from 'axios'
-import { DOMAIN } from './export';
+import axios, { AxiosError } from "axios";
+import { DOMAIN } from "./export";
 
 const instance = axios.create({
-    baseURL: DOMAIN,
-    timeout: 100000
+  baseURL: "https://dashboard.heroku.com/apps/booksellery",
+  timeout: 100000,
 });
 instance.interceptors.request.use(
-    function (config) {
-        return config;
-    }, 
-    function (error : AxiosError) {
-        return Promise.reject(error);
-    }
+  function (config) {
+    return config;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
 );
 instance.interceptors.response.use(
-    function (response) {
-        return response;
-    },
+  function (response) {
+    return response;
+  },
 
-    function (error : AxiosError) {
-        return Promise.reject(error);
-    }
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
 );
 export default instance;
